909971a89e18696a0075e8776279a7b7a4f3ccd1
shiyong <shiyonghm@163.com>
2019-12-08 17:19:55 +0800

    项目初始化

---
+++ src/main/java/com/future/storage/FutureStorageApplication.java
@@ -0,0 +1,13 @@
+package com.future.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureStorageApplication.class, args);
+	}
+
+}
